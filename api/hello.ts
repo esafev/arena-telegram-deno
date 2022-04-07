271c1316b84478de951b5ab03b729c65091e7165
Vlad Esafev <vlad@esafev.com>
2022-04-08 00:15:01 +0400

    add import_map

--- api/hello.ts
+++ api/hello.ts
@@ -1,2 +1,5 @@
-import { text } from "./text";
-export default () => new Response(`Hello, from Deno v${Deno.version.deno}! ${text.test}`);
+#!/usr/bin/env deno run --import-map ../import_map.json
+
+import { text } from 'text';
+
+export default () => new Response(`Hello, from Deno v${Deno.version.deno}!`);
